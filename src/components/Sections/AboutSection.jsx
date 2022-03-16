78f886b212d04dad6678d4683b46008723bc5c1c
Amine Elkhalidy <elkhalidyamine.a@gmail.com>
2022-03-16 10:33:36 +0100

    Adding some slight changes...

--- src/components/Sections/AboutSection.jsx
+++ src/components/Sections/AboutSection.jsx
@@ -4,82 +4,61 @@ import PersonInfo from "../Reusable/PersonInfo";
 
 const PERSONS = [
   {
-    id: 'P1',
+    id: "P1",
     image: AboutImage,
-    name: 'Amine Elkhalidy',
-    description: 'Web developer with extensive knowledge and years of experience, working in web technologies and UI/UX design, delivering quality work.',
-    experienceYears: '08',
-    completedProjectsNumber: '20',
-    companiesNumber: '05',
-    linkToDocument: '',
+    name: "Amine Elkhalidy",
+    description:
+      "Web developer with extensive knowledge and years of experience, working in web technologies and UI/UX design, delivering quality work.",
+    experienceYears: "08",
+    completedProjectsNumber: "20",
+    companiesNumber: "05",
+    linkToDocument: "",
   },
   {
-    id: 'P2',
+    id: "P2",
     image: AboutImage,
-    name: 'Rachid Echatoui',
-    description: 'Web developer with extensive knowledge and years of experience, working in web technologies and UI/UX design, delivering quality work.',
-    experienceYears: '08',
-    completedProjectsNumber: '20',
-    companiesNumber: '05',
-    linkToDocument: ''
-  }
+    name: "Rachid Echatoui",
+    description:
+      "Web developer with extensive knowledge and years of experience, working in web technologies and UI/UX design, delivering quality work.",
+    experienceYears: "08",
+    completedProjectsNumber: "20",
+    companiesNumber: "05",
+    linkToDocument: "",
+  },
 ];
 
-const linkToPortfolio = 'https://amine-elkhalidy-portfolio.netlify.app/';
+const linkToPortfolio = "https://amineelkhalidy.netlify.app/";
 
 const AboutSection = () => {
   return (
     <section className="about section">
-      <h2 className="section__title">About Us</h2>
-      <span className="section__subtitle">Our introduction</span>
+      <h2 className="section__title">معلومات عنا</h2>
+      <span className="section__subtitle">About Us</span>
 
       <div className="about__container container grid">
-        {/* <img src={AboutImage} alt="About Image" className="about__img" />
-
-        <div className="about__data">
-          <p className="about__description">
-            Web developer with extensive knowledge and years of experience,
-            working in web technologies and UI/UX design, delivering quality
-            work.
-          </p>
-
-          <div className="about__info">
-            <div>
-              <span className="about__info-title">08+</span>
-              <span className="about__info-name">
-                Years <br />
-                experience
-              </span>
-            </div>
-
-            <div>
-              <span className="about__info-title">20+</span>
-              <span className="about__info-name">
-                Completed <br /> project
-              </span>
-            </div>
-
-            <div>
-              <span className="about__info-title">05+</span>
-              <span className="about__info-name">
-                Companies <br />
-                worked
-              </span>
-            </div>
-          </div>
-
-          <div className="about__buttons">
-            <a className="button button--flex" download="" href="#">
-              Download CV <i class="ri-download-line button__icon"></i>
-            </a>
-          </div>
-        </div> */}
         {PERSONS.map((person) => (
-          <PersonInfo key={person.id} image={person.image} name={person.name} description={person.description} experienceYears={person.experienceYears} completedProjectsNumber={person.completedProjectsNumber} companiesNumber={person.companiesNumber} linkToPortfolio={person.linkToPortfolio} />
+          <PersonInfo
+            key={person.id}
+            image={person.image}
+            name={person.name}
+            description={person.description}
+            experienceYears={person.experienceYears}
+            completedProjectsNumber={person.completedProjectsNumber}
+            companiesNumber={person.companiesNumber}
+            linkToPortfolio={person.linkToPortfolio}
+          />
         ))}
       </div>
 
-      <p className="about__footer-description">For more information about me, do not hesitate to visit my portfolio:<br /> <span className="about__portfolio-link"><a className="portfolio__link" target="_blank" href={linkToPortfolio}>{linkToPortfolio}</a></span></p>
+      <p className="about__footer-description">
+        (Portfolio) لمزيد من المعلومات عني, يمكنك زيارة موقعي التعريفي
+        <br />
+        <span className="about__portfolio-link">
+          <a className="portfolio__link" target="_blank" href={linkToPortfolio}>
+            {linkToPortfolio}
+          </a>
+        </span>
+      </p>
     </section>
   );
 };
--- src/components/Sections/ContactSection.jsx
+++ src/components/Sections/ContactSection.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 const ContactSection = () => {
   const [state, handleSubmit] = useForm("mjvlwowe");
+
   return (
     <section className="contact section">
-      <h2 className="section__title">Contact Us</h2>
+      <h2 className="section__title">تواصل معنا</h2>
       <span className="section__subtitle">Get in touch</span>
 
       <div className="contact__container container grid">
@@ -13,7 +14,7 @@ const ContactSection = () => {
             <i className="ri-phone-line contact__icon"></i>
 
             <div>
-              <h3 className="contact__title">Call Me</h3>
+              <h3 className="contact__title">الهاتف</h3>
               <span className="contact__subtitle">+212611154307</span>
             </div>
           </div>
@@ -22,7 +23,7 @@ const ContactSection = () => {
             <i className="ri-mail-line contact__icon"></i>
 
             <div>
-              <h3 className="contact__title">Email</h3>
+              <h3 className="contact__title">البريد الالكتروني</h3>
               <span className="contact__subtitle">
                 elkhalidyamine.a@gmail.com
               </span>
@@ -33,9 +34,9 @@ const ContactSection = () => {
             <i className="ri-map-pin-line contact__icon"></i>
 
             <div>
-              <h3 className="contact__title">Location</h3>
+              <h3 className="contact__title">الموقع</h3>
               <span className="contact__subtitle">
-                Sidi Aissa Ben Slimane - Elkelaa Des Sraghna
+                دوار اولاد خالد, جماعة سيدي عيسى بن سليمان - قلعة السراغنة
               </span>
             </div>
           </div>
@@ -49,7 +50,7 @@ const ContactSection = () => {
           <div className="contact__inputs grid">
             <div className="contact__content">
               <label htmlFor="name" className="contact__label">
-                Name
+                اسمك
               </label>
               <input
                 id="name"
@@ -65,9 +66,9 @@ const ContactSection = () => {
               />
             </div>
 
-            <div className="contact__content">
+            <div lang="ar" className="contact__content">
               <label htmlFor="phoneNumber" className="contact__label">
-                Phone
+                رقم هاتفك
               </label>
               <input
                 id="phoneNumber"
@@ -86,14 +87,13 @@ const ContactSection = () => {
 
           <div className="contact__content">
             <label htmlFor="email" className="contact__label">
-              Email
+              بريدك الالكتروني
             </label>
             <input
               id="email"
               name="email"
               type="email"
               className="contact__input"
-              required
             />
             <ValidationError
               prefix="Email"
@@ -104,7 +104,7 @@ const ContactSection = () => {
 
           <div className="contact__content">
             <label htmlFor="message" className="contact__label">
-              Message
+              رسالتك
             </label>
             <textarea
               id="message"
@@ -124,22 +124,13 @@ const ContactSection = () => {
 
           <div>
             <a
-              href="#"
               className="button button--flex"
               type="submit"
               disabled={state.submitting}
             >
-              Send Message <i className="ri-send-plane-line button__icon"></i>
+              <i className="ri-send-plane-line button__icon"></i> ارسال الرسالة
             </a>
           </div>
-
-          {/* <button
-            className="button button--flex"
-            type="submit"
-            disabled={state.submitting}
-          >
-            Send Message <i className="ri-send-plane-line button__icon"></i>
-          </button> */}
         </form>
       </div>
     </section>
