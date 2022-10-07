4276b59597aa1c5f185d805370c12829db5eaf18
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-10-07 12:39:51 +0100

    Fixing the contact page

--- src/components/Sections/ContactSection.jsx
+++ src/components/Sections/ContactSection.jsx
@@ -6,8 +6,8 @@ const ContactSection = () => {
 
   return (
     <section className="contact section">
-      <h2 className="section__title">Contact us</h2>
       <span className="section__subtitle">Get in touch</span>
+      <h2 className="section__title">Contact us</h2>
 
       <div className="contact__container container grid">
         <div>
